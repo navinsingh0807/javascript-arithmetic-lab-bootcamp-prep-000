609384a56e5e2ee22fe2764b51efb9621047844d
Navin <navin.singh0807@gmail.com>
2018-03-13 06:21:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function multiply(x,y){
   return(x*y)
 }
 function divide(x,y){
-  return(x/y)
+  return(x/y);
 }
 function inc(n){
-  return(n+1)
+  return(n+1);
 }
 function dec(n){
   return(n-1)
